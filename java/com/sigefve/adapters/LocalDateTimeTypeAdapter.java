45a1b87e56f3444a48e0bb6d7be3f8d19ca90acb
Aldo Ivan <aldovnlv@gmail.com>
2025-11-02 12:29:52 -0600

    contextos

--- java/com/sigefve/adapters/LocalDateTimeTypeAdapter.java
+++ java/com/sigefve/adapters/LocalDateTimeTypeAdapter.java
@@ -4,8 +4,8 @@ package com.sigefve.adapters;
 // import com.google.gson.JsonObject;
 import com.google.gson.JsonSerializer; //
 import com.google.gson.JsonDeserializer; //
-import com.google.gson.*;
-// import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonDeserializationContext;
 // import com.sun.net.httpserver.HttpExchange;
 // import com.sun.net.httpserver.HttpHandler;
 
