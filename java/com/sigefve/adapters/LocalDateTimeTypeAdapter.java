a91b2ccbd11299defda61dc98a7924b36180fbfd
Aldo Ivan <aldovnlv@gmail.com>
2025-11-02 12:36:30 -0600

    JsonElement

--- java/com/sigefve/adapters/LocalDateTimeTypeAdapter.java
+++ java/com/sigefve/adapters/LocalDateTimeTypeAdapter.java
@@ -6,6 +6,7 @@ import com.google.gson.JsonSerializer; //
 import com.google.gson.JsonDeserializer; //
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonElement;
 // import com.sun.net.httpserver.HttpExchange;
 // import com.sun.net.httpserver.HttpHandler;
 
